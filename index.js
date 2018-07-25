3811854b96fafd78a6910b8b67eda0308b676810
liyinghao666 <928815137@qq.com>
2018-07-26 03:23:56 +0800

    fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = new express();
+const PORT = process.env.PORT || 5000;
 app.use(express.static(__dirname + '/dist'));
 app.get('/',function(req,res){
     res.sendFile('index.html');
@@ -21,7 +22,7 @@ app.get('/api/more.json',function(){
     res.sendFile('api/more.json')
 });
 
-app.listen(3000,function(){
+app.listen(PORT,function(){
     console.log('connected!');
 });
 
